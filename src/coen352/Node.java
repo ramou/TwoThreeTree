fc2bff37dff511fb8f2e782ce1cfeb8c35dbc6c8
Stuart Thiel <stuart.thiel@gmail.com>
2023-03-24 11:49:17 -0400

    Added some comments.

--- src/coen352/Node.java
+++ src/coen352/Node.java
@@ -21,25 +21,31 @@ public class Node {
 
 	public Node add(Integer val) {
 		if(children.isEmpty()) {
-			//we're at a leaf, so we add it to the node
+			//we're at a leaf, so we add it to the node, then re-sort 'cause we're lazy
 			vals.add(val);
 			Collections.sort(vals);
 		} else {
 			Node potentiallySplit = null;
 			int i = 0;
-			for(; i < vals.size() && val >= vals.get(i); ++i) {
-				
-			}
+			//Figure out which child we need to add to (the ith child)
+			for(; i < vals.size() && val.compareTo(vals.get(i)) != -1; ++i) {}
 			
+			//Add to that child and get the returned node, which is potentially promoted
 			potentiallySplit = children.get(i).add(val);
 			
+			//If it's different from what it was, it's promoted and we need to re-integrate that
 			if( children.get(i) != potentiallySplit) {
+				//Add a new value at the ith position since something was promoted
 				vals.add(i,potentiallySplit.vals.get(0));
+				//Remove the old single child
 				children.remove(i);
+				//Add the two replacement children
 				children.addAll(i, potentiallySplit.children);
 			}
 		
 		}
+		
+		//If we're too big, we need to promote something, otherwise we just return ourself
 		return vals.size() > 2?split():this;
 	}
 
